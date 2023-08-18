17639eec701fb5b41774eeed19f6f58e7074d7be
Stacy <stacy.doss@gmail.com>
2023-08-18 10:10:33 -0500

    First commit for working implimentation, (foobarqix)

---
+++ katas/foobarqix/python/foobarqix/foobarqix.py
@@ -0,0 +1,116 @@
+'''
+The FooBarQix kata written in object oriented fashion.
+'''
+
+import re
+
+class FooBarQix():
+    '''
+    Very simple class to contain our implementation.
+
+    We could have implemented this as classmethods as well, and may do so in the
+    future.
+    '''
+
+    def number_devide(self, number):
+        '''
+        Check disivability of number under consideration. Build string 
+        representing the divisability.
+        '''
+        value = ""
+
+        if number % 3 == 0:
+            value = "Foo"
+
+        if number % 5 == 0:
+            value += "Bar"
+
+        if number % 7 == 0:
+            value += "Qix"
+
+        return value
+
+    def digit_substitution(self, number):
+        '''
+        Per kata rules, substitute word for number where needed
+        '''
+        value = ""
+        for digit in str(number):
+            if digit == "3":
+                value += "Foo"
+
+            if digit == "5":
+                value += "Bar"
+
+            if digit == "7":
+                value += "Qix"
+
+        return value
+
+    def digit_substitution2(self, value):
+        '''
+        Per kata step 2, substitute words for numbers and 0 with * as needed.
+        If substitutions have been made, remove anyother digits.
+        '''
+        value = re.sub('3', "Foo", value)
+        value = re.sub('5', "Bar", value)
+        value = re.sub('7', "Fix", value)
+        value = re.sub('0', "*", value)
+        if not re.match('[0-9]', value):
+            value = re.sub('[0-9]', "", value)
+
+        return value
+
+    def translate(self, number):
+        '''
+        From kata ---
+        For any number print the number, unless:
+        If the number is divisible by 3, write “Foo” instead of the number
+        If the number is divisible by 5, add “Bar”
+        If the number is divisible by 7, add “Qix”
+        For each digit 3, 5, 7, add “Foo”, “Bar”, “Qix” in the digits order.
+        '''
+        value = self.number_devide(number)
+        value += self.digit_substitution(number)
+
+        if not value:
+            value = f'{number}'
+
+        return value
+
+    def translate2(self, number):
+        '''
+        From kata (part 2) ---
+        As before, but
+        We have a new business request: we must keep a trace of 0 in numbers, 
+        each 0 must be replaced by char “*”.
+
+        101   => 1*1
+        303   => FooFoo*Foo
+        105   => FooBarQix*Bar
+        10101 => FooQix**
+
+        The test cases implies that We do the division substitutions first.
+        Then iterate over the numbers making the digit substitutions, removing
+        arbitrary numbers if a substitution has been made.
+        That is
+            101 => 101
+            101 => 1*1
+
+            303 => Foo303
+            Foo303 = FooFoo*Foo
+
+            105 => FooBarQix105
+            FooBarQix105 => FooBarQix*Bar
+
+            10101 => FooQix10101
+            FooQix10101 =>FooQix**
+        '''
+        # This gets the prefix
+        value = self.number_devide(number) + str(number)
+        # 303 => Foo303
+
+        # Sub translation for number
+        value = self.digit_substitution2(value)
+
+        return value
---
+++ katas/foobarqix/python/tests/test_all.py
@@ -0,0 +1,70 @@
+'''
+Test for FooBarQix kata
+'''
+
+from foobarqix.foobarqix import FooBarQix
+
+class TestFooBarQix:
+    '''
+    Simple class to bundle our test under.
+    '''
+
+    def test_create(self):
+        '''
+        Intent:
+        As a matter of style, always test that the class under test can create
+        an object.
+        '''
+        cut = FooBarQix()
+        assert isinstance(cut, FooBarQix)
+
+    def test_translate(self):
+        '''
+        Intent:
+        Given any number we sholud be able to apply the FooBarQix rules to get
+        a string representation.
+
+        For any number print the number, unless:
+        If the number is divisible by 3, write “Foo” instead of the number
+        If the number is divisible by 5, add “Bar”
+        If the number is divisible by 7, add “Qix”
+        For each digit 3, 5, 7, add “Foo”, “Bar”, “Qix” in the digits order.
+
+        These test should be easily verifable by inspection.
+        '''
+        cut = FooBarQix()
+        assert cut.translate(1) == '1'
+        assert cut.translate(3) == 'FooFoo'
+        assert cut.translate(5) == 'BarBar'
+        assert cut.translate(7) == 'QixQix'
+        assert cut.translate(15) == 'FooBarBar'
+        assert cut.translate(21) == 'FooQix'
+        assert cut.translate(33) == 'FooFooFoo'
+        assert cut.translate(35) == 'BarQixFooBar'
+        assert cut.translate(53) == 'BarFoo'
+        assert cut.translate(105) == 'FooBarQixBar'
+        assert cut.translate(30) == 'FooBarFoo'
+        assert cut.translate(50) == 'BarBar'
+        assert cut.translate(70) == 'BarQixQix'
+
+    def test_translate2(self):
+        '''
+        Intent:
+        Given any number we sholud be able to apply the FooBarQix rules to get
+        a string representation.
+
+        As before, plus:
+
+        101   => 1*1
+        303   => FooFoo*Foo
+        105   => FooBarQix*Bar
+        10101 => FooQix**
+
+        These test should be asily verifable by inspection.
+        '''
+        cut = FooBarQix()
+        print(f"test {cut.translate2(101)}")
+        assert cut.translate2(101) == '1*1'
+        assert cut.translate2(303) == 'FooFoo*Foo'
+        assert cut.translate2(105) == 'FooBarQix*Bar'
+        assert cut.translate2(10101) == 'FooQix**'
