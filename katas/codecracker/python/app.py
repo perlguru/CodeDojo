057136ea17c7ece5d599f753442e44ad4cbef907
Stacy <stacy.doss@gmail.com>
2023-08-29 17:57:42 -0500

    First gen of app.py

--- katas/codecracker/python/app.py
+++ katas/codecracker/python/app.py
@@ -2,15 +2,20 @@
 Code Cracker kata app
 '''
 
-from codecracker.codecracker import Decode, Encode
+from codecracker.codecracker import Decode, Encode, Key, alphabet
 
 def main():
     '''
     Thin layer to exercise Hello class
     '''
-    message = Encode("Hello, world.")
-    print(message)
-    print(Decode(message))
+    key = Key()
+    key.set_key("".join(alphabet[::-1]))
+    message = "Hello, world."
+    print("Message: ", message)
+    encoded = Encode().encode(key, message)
+    print("Encoded: ", encoded)
+    decoded = Decode().decode(key, encoded)
+    print("Decoded: ", decoded)
 
 if __name__ == "__main__":
     main()
