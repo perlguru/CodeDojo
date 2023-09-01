7c9fff5fe7287710771a7867ae4f925f5a939651
Stacy <stacy.doss@gmail.com>
2023-08-31 20:59:28 -0500

    Added intent fo Board object, and a few test

--- katas/gameoflife/python/life/life.py
+++ katas/gameoflife/python/life/life.py
@@ -12,3 +12,29 @@ class Life():
     We could have implemented this as classmethods as well, and may do so in the
     future.
     '''
+    def __init__(self):
+        self.board = [[]]
+
+class Board():
+    '''
+    Class represents the board or "field". It should be able to do the following:
+    1. Store a board in a 2d list of list (maybe we could use an array)
+    2. Read a board definition from a file, where, '*' indicates a life cell, and '.' indicates a dead cell.
+    3. Print a reperesentation of the boards state, with the above definitions of '*' and '.'
+    4. Based on the rules of the Game of Life, create a new board for the next generation.
+    '''
+
+    def __init__(self):
+        pass
+
+    def read_from_file(self, filename):
+        pass
+
+    def convert_to_2d(self):
+        pass
+
+    def display(self):
+        pass
+
+    def create_next_gen(self):
+        pass
--- katas/gameoflife/python/tests/test_all.py
+++ katas/gameoflife/python/tests/test_all.py
@@ -2,9 +2,7 @@
 Test for Game of Life kata.
 '''
 
-from life.life import Life
-
-
+from life.life import Life, Board
 
 class TestLife:
     '''
@@ -19,3 +17,31 @@ class TestLife:
         '''
         cut = Life()
         assert isinstance(cut, Life)
+
+class TestBoard:
+    '''
+    Simple class to bundle our test under.
+    '''
+
+    def test_create(self):
+        '''
+        Intent:
+        As a matter of style, always test that the class under test can create an 
+        object.
+        '''
+        cut = Board()
+        assert isinstance(cut, Board)
+
+    def test_read_from_file(self):
+        cut = Board()
+        lines = cut.read_from_file("tests\\data\\board1.gol")
+        assert lines == ""
+
+    def test_convert_to_2d(self):
+        assert False
+
+    def test_display(self):
+        assert False
+
+    def test_create_next_gen(self):
+        assert False
