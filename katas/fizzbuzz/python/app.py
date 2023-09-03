aa5b48680546944b8c3d3c2de6e7b7d9ad27d944
Stacy <stacy.doss@gmail.com>
2023-09-03 18:51:23 -0500

    Added profiling.

--- katas/fizzbuzz/python/app.py
+++ katas/fizzbuzz/python/app.py
@@ -2,6 +2,8 @@
 FizzBuzz kata application
 '''
 
+import cProfile
+
 from fizzbuzz.fizzbuzz import FizzBuzz
 
 def main():
@@ -10,12 +12,29 @@ def main():
     '''
     cut = FizzBuzz()
     for number in range(1, 101):
-        print(f'{number} => {cut.translate(number)}')
-
-    print('----------')
+        print(f'{number} => {cut.translate1(number)}')
 
     for number in range(1, 101):
         print(f'{number} => {cut.translate2(number)}')
 
+    for number in range(1, 101):
+        print(f'{number} => {cut.translate3(number)}')
+
+def profile():
+    '''
+    Thin layer to exercise FizzBuzz class
+    '''
+    cut = FizzBuzz()
+    for dummy in range(0,10000):
+        for number in range(1, 101):
+            cut.translate1(number)
+
+        for number in range(1, 101):
+            cut.translate2(number)
+
+        for number in range(1, 101):
+            cut.translate3(number)
+
 if __name__ == "__main__":
     main()
+    cProfile.run('profile()')
