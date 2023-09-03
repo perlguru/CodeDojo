379d405c28c1b440e56e3428f18dece93b7daa48
Stacy <stacy.doss@gmail.com>
2023-09-03 17:40:32 -0500

    Added a second "glider" board, and slightly refactored app.py to run a board based on a filename

--- katas/gameoflife/python/app.py
+++ katas/gameoflife/python/app.py
@@ -6,15 +6,24 @@ from time import sleep
 from life.life import Board
 
 BOARD1 = os.getcwd() + "\\" + "tests\\data\\board1.gol"
+BOARD2 = os.getcwd() + "\\" + "tests\\data\\board2.gol"
 
 def main():
     '''
     Thin layer to exercise Life class
     '''
+    run_board(BOARD1)
+    run_board(BOARD2)
+
+def run_board(golboard):
+    '''
+    Runs a board for 20 generations
+    '''
     os.system('clear')
     cut = Board()
-    cut.load_from_file(BOARD1)
+    cut.load_from_file(golboard)
     cut.display()
+
     for dummy in range(0,20):
         cut = cut.compute_next()
         sleep(0.5)
