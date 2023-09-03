b27eaa555555d4a2750840c9397bf21d639c7651
Stacy <stacy.doss@gmail.com>
2023-09-03 17:17:44 -0500

    Made this a little more interesting by profiling the 2 methods

--- katas/leapyears/python/app.py
+++ katas/leapyears/python/app.py
@@ -1,16 +1,28 @@
 '''
 Hello World kata app
 '''
-
+import cProfile
 from leapyear.leapyear import LeapYear
 
 def main():
     '''
-    Thin layer to exercise LeapYear class
+    Thin layer to exercise LeapYear class, and profile the 2 methods
+
+    A lot of iterations are needed to detect observable difference.
+
+    My results show that my leapyear1 is slightly faster than leapyear2
+
+       ncalls  tottime  percall  cumtime  percall filename:lineno(function)
+     25010000    4.942    0.000    4.942    0.000 leapyear.py:12(__init__)
+     25010000   17.528    0.000   17.528    0.000 leapyear.py:18(leapyear1)
+     25010000   17.836    0.000   17.836    0.000 leapyear.py:42(leapyear2)
+
     '''
-    for testdate in range(1500,4001):
-        testdate = LeapYear(testdate)
-        print(testdate.year, testdate.leapyear1(), testdate.leapyear2())
+    for dummy in range(0,10000):
+        for testdate in range(1500,4001):
+            dut = LeapYear(testdate)
+            dut.leapyear1()
+            dut.leapyear2()
 
 if __name__ == "__main__":
-    main()
+    cProfile.run('main()')
