88de5f67d276f38445582db3d8b1af90218a854b
Stacy <stacy.doss@gmail.com>
2023-09-03 17:00:14 -0500

    Added basic app

--- katas/gameoflife/python/app.py
+++ katas/gameoflife/python/app.py
@@ -1,13 +1,26 @@
 '''
 Game of Life kata app
 '''
+import os
+from time import sleep
+from life.life import Board
 
-#from life.life import Life
+BOARD1 = os.getcwd() + "\\" + "tests\\data\\board1.gol"
 
 def main():
     '''
     Thin layer to exercise Life class
     '''
+    os.system('clear')
+    cut = Board()
+    cut.load_from_file(BOARD1)
+    cut.display()
+    for i in range(0,20):
+        cut = cut.compute_next()
+        sleep(0.5)
+        os.system('clear')
+        cut.display()
+
 
 if __name__ == "__main__":
     main()
