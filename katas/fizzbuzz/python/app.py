8ae6715deb29f9b68fabeabca0b14e02b75dc99e
Stacy <stacy.doss@gmail.com>
2023-09-03 19:03:07 -0500

    Added profiling info

--- katas/fizzbuzz/python/app.py
+++ katas/fizzbuzz/python/app.py
@@ -22,7 +22,18 @@ def main():
 
 def profile():
     '''
-    Thin layer to exercise FizzBuzz class
+    Thin layer to profile different FizzBuzz translations.
+
+    A lot of iterations are needed to detect observable difference.
+
+    My results show that my translate1 2x faster than translate3 and translate3
+    5x faster than translate 2.
+
+       ncalls  tottime  percall  cumtime  percall filename:lineno(function)
+      1000000    0.456    0.000    0.456    0.000 fizzbuzz.py:15(translate1)
+      1000000    1.426    0.000    5.063    0.000 fizzbuzz.py:36(translate2)
+      1000000    0.624    0.000    1.061    0.000 fizzbuzz.py:71(translate3)
+      1000000    0.437    0.000    0.437    0.000 fizzbuzz.py:75(<lambda>)
     '''
     cut = FizzBuzz()
     for dummy in range(0,10000):
