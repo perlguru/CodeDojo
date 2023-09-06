2d929c5b94a7afceb5c9d1171601a6d1564cb137
Stacy <stacy.doss@gmail.com>
2023-09-05 22:16:06 -0500

    Added test for misplaced and mixed. Completed implementation for compare_guess_to_secret (tough). Will likely need to revistit this implementation. Testing may be able to be improved as well.

--- katas/mastermind/python/mastermind/mastermind.py
+++ katas/mastermind/python/mastermind/mastermind.py
@@ -56,11 +56,40 @@ class Mastermind():
     def compare_guess_to_secret(self, guess=None, secret=None):
         '''
         Compare a guess to the secret.
+
+        We could probably use a mix of map/reduce here. Leaving that 
+        refinement for a rainy day.
         '''
         if not guess:
-            guess = self.guess
+            guess = self.guess.copy()
 
         if not secret:
-            secret = self.secret
+            secret = self.secret.copy()
 
-        return guess == secret
+        # guess and secret must be the same length
+        assert len(guess) == len(secret)
+
+
+        # Get colors in correct spot
+        correct = []
+        for i, color in enumerate(guess):
+            if color == secret[i]:
+                correct.append(i)
+
+        # Remove the correct guess from the guess and secret list
+        for i in reversed(correct):
+            guess.pop(i)
+            secret.pop(i)
+
+        # Check the remaining guesses with secret
+        misplaced = 0
+        for color in guess:
+            misplaced += secret.count(color)
+            try:
+                while secret.index(color):
+                    secret.remove(color)
+            except ValueError:
+                pass
+
+        # Return our findings
+        return [len(correct), misplaced]
--- katas/mastermind/python/tests/test_all.py
+++ katas/mastermind/python/tests/test_all.py
@@ -42,7 +42,7 @@ class TestMastermind:
 
         assert valid == 4
 
-    def test_compare_guess_to_secret(self):
+    def test_compare_guess_to_secret_all_correct(self):
         '''
         Compare a guess to secret
         '''
@@ -50,6 +50,36 @@ class TestMastermind:
         cut.set_secret(['red', 'blue', 'green'])
         cut.set_guess(['red', 'blue', 'green'])
         assert cut.compare_guess_to_secret()
-        assert cut.compare_guess_to_secret(guess=['red', 'blue', 'green'])
-        assert cut.compare_guess_to_secret(secret=['red', 'blue', 'green'])
-        assert cut.compare_guess_to_secret(guess=['red', 'blue', 'green'], secret=['red', 'blue', 'green'])
+        assert cut.compare_guess_to_secret(guess=['red', 'blue', 'green']) == [3, 0]
+        assert cut.compare_guess_to_secret(secret=['red', 'blue', 'green']) == [3, 0]
+        assert cut.compare_guess_to_secret(guess=['red', 'blue', 'green'], secret=['red', 'blue', 'green']) == [3, 0]
+
+    def test_compare_guess_to_secret_all_misplaced(self):
+        '''
+        Compare a guess to secret
+        '''
+        cut = Mastermind()
+        cut.set_secret(['red', 'blue', 'green'])
+        cut.set_guess(['blue', 'green', 'red'])
+        assert cut.compare_guess_to_secret()
+        assert cut.compare_guess_to_secret(guess=['blue', 'green', 'red']) == [0, 3]
+        assert cut.compare_guess_to_secret(secret=['red', 'blue', 'green']) == [0, 3]
+        assert cut.compare_guess_to_secret(guess=['blue', 'green', 'red'], secret=['red', 'blue', 'green']) == [0, 3]
+
+    def test_compare_guess_to_secret_mixed(self):
+        '''
+        Compare a guess to secret
+        '''
+        cut = Mastermind()
+        cut.set_secret(['red', 'blue', 'green'])
+        cut.set_guess(['blue', 'blue', 'blue'])
+        assert cut.compare_guess_to_secret() == [1, 0]
+        assert cut.compare_guess_to_secret(guess=['blue', 'blue', 'red']) == [1, 1]
+        assert cut.compare_guess_to_secret(secret=['red', 'blue', 'green']) == [1, 0]
+        assert cut.compare_guess_to_secret(guess=['blue', 'blue', 'red'], secret=['red', 'blue', 'green']) == [1, 1]
+        assert cut.compare_guess_to_secret(guess=['blue', 'blue', 'green'], secret=['red', 'blue', 'green']) == [2, 0]
+        assert cut.compare_guess_to_secret(guess=['red', 'blue', 'red'], secret=['red', 'blue', 'green']) == [2, 0]
+        assert cut.compare_guess_to_secret(guess=['blue', 'blue', 'blue'], secret=['red', 'blue', 'green']) == [1, 0]
+        assert cut.compare_guess_to_secret(guess=['blue', 'blue', 'blue', 'blue'], secret=['red', 'blue', 'blue', 'green']) == [2, 0]
+        assert cut.compare_guess_to_secret(guess=['blue', 'blue', 'blue', 'blue'], secret=['red', 'blue', 'green', 'blue']) == [2, 0]
+        assert cut.compare_guess_to_secret(guess=['blue', 'blue', 'blue', 'green'], secret=['red', 'blue', 'green', 'blue']) == [1, 2]
