3c283cc107a579bdcee1e9bf7fad8a1c0101574a
Stacy <stacy.doss@gmail.com>
2023-09-06 02:26:48 -0500

    Added totally contrived app code. Might make it a IHM model in the future

--- katas/mastermind/python/app.py
+++ katas/mastermind/python/app.py
@@ -6,10 +6,26 @@ from mastermind.mastermind import Mastermind
 
 def main():
     '''
-    Thin layer to exercise Hello class
+    Thin layer to exercise Mastermind class
+
+    Totally faked :)
     '''
+    print()
+    print('A typical session')
+    print(['Secret'], ['Guess'], ['Result'])
     cut = Mastermind()
-    assert cut
+    cut.set_secret(['red', 'blue', 'green'])
+    cut.set_guess(['blue', 'green', 'red'])
+    result = cut.compare_guess_to_secret()
+    print(cut.get_secret(), cut.get_guess(), result)
+    cut.set_guess(['red', 'green', 'blue'])
+    result = cut.compare_guess_to_secret()
+    print(cut.get_secret(), cut.get_guess(), result)
+    cut.set_guess(['red', 'blue', 'green'])
+    result = cut.compare_guess_to_secret()
+    print(cut.get_secret(), cut.get_guess(), result)
+    if cut.get_secret() == cut.get_guess():
+        print("Solved")
 
 if __name__ == "__main__":
     main()
