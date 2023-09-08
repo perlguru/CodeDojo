d9297ec3c4232b3d71bdec76082cd673eabc0f10
Stacy <stacy.doss@gmail.com>
2023-09-07 21:44:45 -0500

    Added test for get board and accept play. This requiered implementation of get_board, accept_play and rotate_player

--- katas/tictactoe/python/tests/test_all.py
+++ katas/tictactoe/python/tests/test_all.py
@@ -18,6 +18,25 @@ class TestTTT:
         cut = TTT()
         assert isinstance(cut, TTT)
 
+    def test_get_board(self):
+        '''
+        Intent:
+        Any new TicTacToe game should start with an empty board.
+        '''
+        cut = TTT()
+        assert cut.get_board() == [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+
+    def test_accept_play(self):
+        '''
+        Intent:
+        Test simple board display.
+        '''
+        cut = TTT()
+        cut.accept_play(5)
+        assert cut.get_board() == [[0, 0, 0], [0, "X", 0], [0, 0, 0]]
+        cut.accept_play(7)
+        assert cut.get_board() == [[0, 0, 0], [0, "X", 0], ["O", 0, 0]]
+
     def test_print_board(self):
         '''
         Intent:
--- katas/tictactoe/python/ttt/ttt.py
+++ katas/tictactoe/python/ttt/ttt.py
@@ -11,6 +11,30 @@ class TTT():
         '''
         Initialize instance
         '''
+        self.board = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+        self.player = "X"
+
+    def get_board(self):
+        '''
+        Print the current board to stdout.
+        '''
+        return self.board
+
+    def rotate_player(self):
+        current = self.player
+
+        if self.player == "X":
+            self.player = "O"
+
+        return current
+
+    def accept_play(self, pos):
+
+        '''
+        Print the current board to stdout.
+        '''
+        line = int((pos - 1)/ 3)
+        self.board[line][pos % 3 - 1] = self.rotate_player()
 
     def print_board(self):
         '''
