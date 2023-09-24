d00e2a90f2f58ebf1ecca888f965aa551b29a750
Stacy <stacy.doss@gmail.com>
2023-09-24 05:13:20 -0500

    Minor changes due to refactoring

--- katas/minesweeper/python/app.py
+++ katas/minesweeper/python/app.py
@@ -1,26 +1,23 @@
-'''
+"""
 MineSweeper kata app
-'''
+"""
 
 from minesweeper.minesweeper import MineSweeper
 
 def main():
-    '''
+    """
     Thin layer to exercise MineSweeper class
-    '''
+    """
     cut = MineSweeper()
-    cut.set_board(cut.readfile("tests\\data\\board1.ms"))
+    cut.board.set_board(cut.board.readfile("tests\\data\\board1.ms"))
     print("Initial")
-    for row in cut.board:
-        for col in row:
-            print(col, end = '')
-        print()
+    cut.board.display()
 
-    cut.calculate()
 
     print()
     print("Solved")
-    cut.print_solved()
+    cut.board.calculate()
+    cut.board.print_solved()
 
 if __name__ == "__main__":
     main()
